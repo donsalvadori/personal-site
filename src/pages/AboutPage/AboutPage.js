7aba5617540231644bbb308dc3f8bd3c1617edf3
Chris Salvi <csalvi42@gmail.com>
2018-04-09 11:20:55 -0700

    added social icon and added about info

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -30,17 +30,17 @@ const AboutPage = (props, context) => {
             </p>
             <p>
               Constantly striving to better myself as a professional and full-time human-being {' '}
-              by persuing my many passions. I stay close to
+              by persuing my many passions. I am close to
               the programming community by attending meetups in PDX, engaging in discussions through social media,
               and purchasing educational content to stay fresh as a developer. 
             </p>
             <p> 
               I also like to write comedy, train as a boxer, read fiction, obsess over 1st party Nintendo franchises,
-              and shoot photography with my Olympus camera. 
+              and shoot photography with my Olympus camera. I also am conversational in Brazilian portuguese, 
+              então fique à vontade para falar comigo ;).
             </p>
             <p>
-              I built this responsive portfolio site from scratch,
-              using only best practices with React and responsive design.
+              I built this responsive portfolio site using only best practices with React and responsive design.
             </p>
             <p>
               Modern Javascript, React-Redux, Node.js, Docker, and
